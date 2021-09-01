2bb9d3deadebb94a27db019b265adc13225adc2e
Saulo Dtarso <skdtarso@gmail.com>
2021-09-01 14:32:20 -0300

    Initializing project

---
+++ gendo-test/src/App.vue
@@ -0,0 +1,5 @@
+<template>
+  <router-view/>
+</template>
+
+<style src="@/assets/scss/main.scss" lang="scss"></style>
---
+++ gendo-test/src/router/index.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home/Home.vue'
+import Profile from '../views/Profile/Profile.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/profile',
+    name: 'Profile',
+    component: Profile
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
