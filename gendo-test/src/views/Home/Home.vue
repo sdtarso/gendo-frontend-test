e29afcc5b465849108f43ea871a71dde3128f47e
Saulo Dtarso <skdtarso@gmail.com>
2021-09-01 14:32:35 -0300

    Creting view files

---
+++ gendo-test/src/views/Home/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <main class="homepage">
+    <div class="container">
+      <router-link class="btn btn--block btn--primary" to="profile">LOGIN</router-link>
+    </div>
+  </main>
+</template>
+
+<script>
+
+export default {
+  name: 'Home'
+}
+</script>
+
+<style lang="scss">
+.homepage {
+  width: 100vw;
+  height: 100vh;
+  @include flex
+}
+</style>
---
+++ gendo-test/src/views/Profile/Profile.vue
@@ -0,0 +1,28 @@
+<template>
+  <main>
+    <section class="header">
+      <nav>
+        <router-link to="/">
+          <!-- <img src="@/assets/imagens/github-profiles.png" alt="GitHub Profiles"> -->
+        </router-link>
+      </nav>
+      <div class="bio"></div>
+      <div class="navtabs">
+        <ul class="listinline">
+          <li class="listinline__item">
+            <router-link to="/">Repos</router-link>
+          </li>
+          <li class="listinline__item">
+            <router-link to="/stared">Stared</router-link>
+          </li>
+        </ul>
+      </div>
+    </section>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "Profile",
+};
+</script>
