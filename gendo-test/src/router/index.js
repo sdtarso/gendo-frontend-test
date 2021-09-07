cd31bf38807226e90487e61f72919fa83621b8f4
Saulo Dtarso <skdtarso@gmail.com>
2021-09-06 23:12:58 -0300

    Creating nested routes for profile list

--- gendo-test/src/router/index.js
+++ gendo-test/src/router/index.js
@@ -2,12 +2,26 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Login from '../views/Auth/Login.vue'
 import Auth from '../views/Auth/Auth.vue'
 import Profile from '../views/Profile/Profile.vue'
+import Repositories from '../views/Profile/Repositories.vue'
+import Starred from '../views/Profile/Starred.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Profile',
-    component: Profile
+    component: Profile,
+    children: [
+      {
+        path: '/repositories',
+        name: 'Repositories',
+        component: Repositories
+      },
+      {
+        path: '/starred',
+        name: 'Starred',
+        component: Starred
+      },
+    ]
   },
   {
     path: '/login',
